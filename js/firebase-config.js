c72ce6a02f63643f72e719a030ec7ef11cd62e9e
Julia <juliagabriellems1@gmail.com>
2026-08-15 14:12:09 -0300

    Configure Firebase and Google Calendar integration

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDoIY-m2gwtD9y4KzLzIF-YBVxxJ1GXLjk",
+  authDomain: "julianotes-21925.firebaseapp.com",
+  projectId: "julianotes-21925",
+  storageBucket: "julianotes-21925.firebasestorage.app",
+  messagingSenderId: "289279515291",
+  appId: "1:289279515291:web:7cd2e39f94f088a892443e"
 };
 
 export const hasFirebaseConfig = Object.values(firebaseConfig).every(Boolean);
-
-
